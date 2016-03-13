f07b96d06fa490ade9dfb0af4ce1c823fe4168b3
William-De71 <williamderen@gmail.com>
2016-03-13 15:39:10 +0100

    Ajout fichier fenprincipale.h et fenprincipale.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <QApplication>
-#include "FenPrincipale.h"
+#include <QTranslator>
+#include <QLocale>
+#include <QLibraryInfo>
+#include "fenprincipale.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    // Traduction des chaînes prédéfinies par Qt dans notre langue
     QString locale = QLocale::system().name().section('_', 0, 0);
     QTranslator translator;
     translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     app.installTranslator(&translator);
 
+    // Ouverture de la fenêtre principale du navigateur
     FenPrincipale fenetre;
     fenetre.show();
 
